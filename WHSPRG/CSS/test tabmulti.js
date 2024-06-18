06e0a06ab9cf432d5a27f5b89871c39044bec201
AlxAlbr <77879821+AlxAlbr@users.noreply.github.com>
2024-06-18 14:35:13 +0200

    Add files via upload

---
+++ WHSPRG/CSS/test tabmulti.js
@@ -0,0 +1,24 @@
+<script>
+// déclaration du tableau multidimentionnel 
+
+var TabSeg = new Array();
+
+TabSeg['début'] = new Array();
+TabSeg['fin'] = new Array();
+TabSeg['texte'] = new Array();
+TabSeg['temp'] = new Array();
+TabSeg['prob'] = new Array();
+
+/*document.write('Le langage ' + TabSeg['langage'][1] + ' est un langage '
+                  + TabSeg['type'][1] + ' ' + TabSeg['cote'][1]); 
+*/
+
+
+function poussetab(){
+    TabSeg.push(0.0,10,"Une première portion de dix secondes",1,0.55555)
+    TabSeg.push(10.0,15,"Une deuxième portion de quinze secondes",1,0.55555)
+ 
+ alert(Tabseg[1]['fin'])
+}
+
+</script>
